6b2c308d7ca34d2d59b76064528f7768f60b4c60
Kenjiro Nagao <toto1010@hotmail.com>
2018-05-19 16:03:35 +0900

    Remove unused usings

--- WpfTableEditor/Samples/ViewModels/TreeItemViewModel.cs
+++ WpfTableEditor/Samples/ViewModels/TreeItemViewModel.cs
@@ -4,12 +4,8 @@
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 //////////////////////////////////////////////////
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using unvell.ReoGrid.WpfTableEditor.Samples.ViewModels;
 using WpfTableEditor.TableEditors.ViewModels;
 

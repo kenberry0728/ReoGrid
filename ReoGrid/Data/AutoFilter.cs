981b71c99290f420805b81872ef744645e836170
長尾 健次郎 <toto1010@hotmail.com>
2018-05-22 00:49:12 +0900

    temp imp of autofilter

--- ReoGrid/Data/AutoFilter.cs
+++ ReoGrid/Data/AutoFilter.cs
@@ -562,11 +562,25 @@ namespace unvell.ReoGrid.Data
 #if WINFORM
 				unvell.ReoGrid.WinForm.ColumnFilterContextMenu.ShowFilterPanel(headerBody, (System.Drawing.Point)point);
 #elif WPF
-				var ctx = new System.Windows.Controls.ContextMenu();
-				ctx.Items.Add(new System.Windows.Controls.MenuItem() { Header = "Item" });
-				ctx.IsOpen = true;
+                var popup = new System.Windows.Controls.Primitives.Popup()
+                {
+                    Child = new WPF.FilterGUI(headerBody),
+                    Placement = System.Windows.Controls.Primitives.PlacementMode.Mouse,
+                    IsOpen = true,
+                    StaysOpen = true
+                };
+                popup.Child.Focus();
+                System.Windows.DependencyPropertyChangedEventHandler handler = null;
+                handler = (x, y) =>
+                {
+                    popup.IsOpen = false;
+                    popup.StaysOpen = false;
+                    popup.IsKeyboardFocusWithinChanged -= handler;
+                };
+                popup.IsKeyboardFocusWithinChanged += handler;
+
 #endif // WPF
-				return true;
+                return true;
 			}
 
 			return false;
---
+++ ReoGrid/WPF/FilterGUI.xaml.cs
@@ -0,0 +1,50 @@
+﻿//////////////////////////////////////////////////
+// WpfTableEditor
+// Copyright (c) 2018 Kenjiro.Nagao
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+//////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static unvell.ReoGrid.Data.AutoColumnFilter;
+
+namespace unvell.ReoGrid.WPF
+{
+    /// <summary>
+    /// Interaction logic for FilterGUI.xaml
+    /// </summary>
+    public partial class FilterGUI : UserControl
+    {
+        private readonly AutoColumnFilterBody headerBody;
+
+        public FilterGUI(AutoColumnFilterBody headerBody)
+        {
+            InitializeComponent();
+            this.headerBody = headerBody;
+        }
+        
+        private void AtoZButtonClick(object sender, RoutedEventArgs e)
+        {
+            var worksheet = this.headerBody.ColumnHeader.Worksheet;
+            worksheet.SortColumn(headerBody.ColumnHeader.Index, headerBody.autoFilter.ApplyRange, SortOrder.Ascending);
+        }
+
+        private void ZtoAButtonClick(object sender, RoutedEventArgs e)
+        {
+            var worksheet = this.headerBody.ColumnHeader.Worksheet;
+            worksheet.SortColumn(headerBody.ColumnHeader.Index, headerBody.autoFilter.ApplyRange, SortOrder.Descending);
+        }
+    }
+}
